95b03eb8d5a4591280682c7e7fbdee03128ed4eb
M.Anas Kamran <studanns1@gmail.com>
2026-02-10 20:07:19 +0500

    Add files via upload

---
+++ Encapsulation/Bank.java
@@ -0,0 +1,27 @@
+package bank;
+class Account{
+public String name;
+protected String email;
+private String password;
+
+public String getPassword(){
+    return this.password;
+}
+public void setPassword(String pass){
+    this.password = pass;
+}
+}
+public class Bank{
+    public static void main(String[] args){
+        Account account1 = new Account();
+        account1.name = "Meow meow";
+        account1.email = "meow@gmail.com";
+        account1.setPassword("abcd");
+        String retrievedPassword = account1.getPassword();
+        System.out.println("Email: " + account1.email);
+        System.out.println("Retireved password : " + retrievedPassword);
+//        System.out.println(account1.getPassword());
+
+    }
+}
+
