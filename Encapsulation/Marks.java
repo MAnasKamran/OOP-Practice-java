617d8c9d4926eebb5878d73b5c778a6be8b0cb70
M.Anas Kamran <studanns1@gmail.com>
2026-02-14 18:19:39 +0500

    Update Marks.java

--- Encapsulation/Marks.java
+++ Encapsulation/Marks.java
@@ -1,19 +1,22 @@
-public class Marks {
-    private double A1;
-    private  double B2;
-    private  double C3;
 
-    public Marks(){
+class Marks {
+    private double A1;
+    private double B2;
+    private double C3;
+
+    public Marks() {
         double A1 = 0;
         double B2 = 0;
         double C3 = 0;
     }
-    public Marks(double a1, double b2, double c3){
+
+    public Marks(double a1, double b2, double c3) {
         A1 = a1;
         B2 = b2;
         C3 = c3;
     }
-    public void setA1(double a1){
+
+    public void setA1(double a1) {
         A1 = a1;
     }
 
@@ -24,7 +27,8 @@ public class Marks {
     public void setC3(double c3) {
         C3 = c3;
     }
-    public double getA1(){
+
+    public double getA1() {
         return A1;
     }
 
@@ -36,21 +40,26 @@ public class Marks {
         return C3;
     }
 
-    public double calculateSum(){
+    public double calculateSum() {
         return (A1 + B2 + C3);
     }
-    public double calculateAverage(){
+
+    public double calculateAverage() {
         return (A1 + B2 + C3) / 3;
     }
-    public static void main(String [] args){
-        Marks m1 = new Marks();
-        m1.setA1(90.5);
-        m1.setB2(92.4);
-        m1.setC3(95.2);
-        System.out.println("A1 = " + m1.getA1());
-        System.out.println("B2 = " + m1.getB2());
-        System.out.println("C3 = " + m1.getC3());
-        System.out.println("Sum = " + m1.calculateSum());
-        System.out.println("Average = " + m1.calculateAverage());
-    }
 }
+
+    public class marksRunner {
+
+        public static void main(String[] args) {
+            Marks m1 = new Marks();
+            m1.setA1(90.5);
+            m1.setB2(92.4);
+            m1.setC3(95.2);
+            System.out.println("A1 = " + m1.getA1());
+            System.out.println("B2 = " + m1.getB2());
+            System.out.println("C3 = " + m1.getC3());
+            System.out.println("Sum = " + m1.calculateSum());
+            System.out.println("Average = " + m1.calculateAverage());
+        }
+    }
